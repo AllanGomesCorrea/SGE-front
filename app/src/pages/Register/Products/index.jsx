dc0478247dda3f299aabb8a7373383709e8e6c48
allan <allan.correa-ext@rtm.net.br>
2024-11-05 08:37:12 -0300

    Update Produtcs with new table

--- app/src/pages/Register/Products/index.jsx
+++ app/src/pages/Register/Products/index.jsx
@@ -36,18 +36,15 @@ function Products() {
         fetchProducts();
     }, [fetchProducts]);
 
-    const handleItemsPerPageChange = (e) => {
-        setItemsPerPage(Number(e.target.value));
-        setCurrentPage(1);
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
     };
 
-    const handlePreviousPage = () => {
-        setCurrentPage((prev) => Math.max(prev - 1, 1));
+    const handleRowsPerPageChange = (event) => {
+        setItemsPerPage(parseInt(event.target.value, 10));
+        setCurrentPage(1); // Reinicia para a primeira página ao mudar o número de itens por página
     };
 
-    const handleNextPage = () => {
-        setCurrentPage((prev) => Math.min(prev + 1, totalPages));
-    };
 
     return (
         <S.PageContainer>
@@ -56,21 +53,15 @@ function Products() {
                 <Sidebar />
                 <S.MainContent>
                     <ProductsModalButton fetchProducts={fetchProducts} />
-                    <S.Select onChange={handleItemsPerPageChange} value={itemsPerPage}>
-                        <option value={10}>10</option>
-                        <option value={20}>20</option>
-                        <option value={50}>50</option>
-                    </S.Select>
-                    <ProductsTable products={products} />
-                    <S.PaginationContainer>
-                        <S.Button onClick={handlePreviousPage} disabled={currentPage === 1}>
-                            Anterior
-                        </S.Button>
-                        <S.PageIndicator>Página {currentPage} de {totalPages}</S.PageIndicator>
-                        <S.Button onClick={handleNextPage} disabled={currentPage === totalPages}>
-                            Próxima
-                        </S.Button>
-                    </S.PaginationContainer>
+                    
+                    <ProductsTable 
+                        products={products}
+                        page={currentPage}
+                        rowsPerPage={itemsPerPage}
+                        totalPages={totalPages}
+                        onPageChange={handlePageChange}
+                        onRowsPerPageChange={handleRowsPerPageChange}
+                    />                    
                 </S.MainContent>
             </S.ContentWrapper>
             <Footer />
